aa4353e89a122715fff341c35f0158fcc190382c
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-03-25 13:12:04 -0400

    Add n-grams for a range of n

--- R/ngram-tokenizers.R
+++ R/ngram-tokenizers.R
@@ -10,7 +10,10 @@
 #' contiguous subsequence of \code{n} words. }
 #' \item{\code{tokenize_skip_ngrams}:}{Skip n-grams. A subsequence of \code{n}
 #' words which are at most a gap of \code{k} words between them. The skip
-#' n-grams will be calculated for all values from \code{0} to \code{k}. } }
+#' n-grams will be calculated for all values from \code{0} to \code{k}. }
+#' \item{\code{tokenize_range_ngrams}:}{Shingled n-grams for a range of
+#' \code{n}. This will compute shingled n-grams for every value of between
+#' \code{n_min} (which must be at least 1) and \code{n}.} }
 #'
 #' These functions will strip all punctuation and normalize all whitespace to a
 #' single space character.
@@ -24,6 +27,9 @@
 #' @param k For the skip n-gram tokenizer, the maximum skip distance between
 #'   words. The function will compute all skip n-grams between \code{0} and
 #'   \code{k}.
+#' @param n_min For the tokenizer that computes n-grams for a range of \code{n},
+#'   the minimum value of \code{n}. This must be an integer greater than or
+#'   equal to 1.
 #' @param lowercase Should the tokens be made lower case?
 #' @examples
 #' song <-  paste0("How many roads must a man walk down\n",
@@ -38,6 +44,8 @@
 #'
 #' tokenize_ngrams(song, n = 4) %>% head(10)
 #' tokenize_skip_ngrams(song, n = 4, k = 2) %>% head(10)
+#' tokenize_range_ngrams(song, n = 8, n_min = 1) %>% head(5)
+#' tokenize_range_ngrams(song, n = 8, n_min = 1) %>% tail(5)
 #'
 #' song %>%
 #'   tokenize_sentences() %>%
@@ -69,4 +77,26 @@ tokenize_skip_ngrams <- function(x, lowercase = TRUE, n = 3, k = 1) {
     out <- lapply(words, skip_ngrams, n = n, k = k)
 
   out
+}
+
+#' @export
+#' @rdname ngram-tokenizers
+tokenize_range_ngrams <- function(x, n = 3, n_min = 1, lowercase = TRUE) {
+  stopifnot(n_min >= 1, n >= n_min)
+  n_range <- n:n_min
+
+  words <- tokenize_words(x, lowercase = lowercase)
+
+  if (is.list(words)) {
+    out <- lapply(words, apply_range, n_range)
+  } else if (is.character(words)) {
+    out <- apply_range(words, n_range)
+  }
+
+  out
+}
+
+apply_range <- function(x, n_range) {
+  lapply(n_range, function(n) { shingle_ngrams(x, n = n) }) %>%
+    unlist(recursive = FALSE, use.names = FALSE)
 }
