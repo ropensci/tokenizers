a963642c4c26c3f080318de9d140cd9a83b6552c
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-08-25 13:10:47 -0400

    Shingled characters tokenizer

---
+++ R/character-shingles-tokenizers.R
@@ -0,0 +1,53 @@
+#' Character shingle tokenizers
+#'
+#' The character shingle tokenizer functions like an n-gram tokenizer, except
+#' the units that are shingled are characters instead of words. Options to the
+#' function let you determine whether non-alphanumeric characters like
+#' punctuation should be retained or discarded.
+#'
+#' @param x A character vector or a list of character vectors to be tokenized
+#'   into character shingles. If \code{x} is a character vector, it can be of
+#'   any length, and each element will be tokenized separately. If \code{x} is a
+#'   list of character vectors, each element of the list should have a length of
+#'   1.
+#' @param n The number of characters in each shingle. This must be an integer
+#'   greater than or equal to 1.
+#' @param n_min This must be an integer greater than or equal to 1, and less
+#'   than or equal to \code{n}.
+#' @param lowercase Should the characters be made lower case?
+#' @param strip_non_alphanum Should punctuation and white space be stripped?
+#' @param simplify \code{FALSE} by default so that a consistent value is
+#'   returned regardless of length of input. If \code{TRUE}, then an input with
+#'   a single element will return a character vector of tokens instead of a
+#'   list.
+#'
+#' @return A list of character vectors containing the tokens, with one element
+#'   in the list for each element that was passed as input. If \code{simplify =
+#'   TRUE} and only a single element was passed as input, then the output is a
+#'   character vector of tokens.
+#'
+#' @examples
+#' x <- c("Now is the hour of our discontent")
+#' tokenize_character_shingles(x)
+#' tokenize_character_shingles(x, n = 5)
+#' tokenize_character_shingles(x, n = 5, strip_non_alphanum = FALSE)
+#' tokenize_character_shingles(x, n = 5, n_min = 3, strip_non_alphanum = FALSE)
+#'
+#' @export
+#' @rdname shingle-tokenizers
+tokenize_character_shingles <- function(x, n = 3L, n_min = n,
+                                        lowercase = TRUE,
+                                        strip_non_alphanum = TRUE,
+                                        simplify = FALSE) {
+  check_input(x)
+  named <- names(x)
+  if (n < n_min || n_min <= 0)
+    stop("n and n_min must be integers, and n_min must be less than ",
+         "n and greater than 1.")
+  chars <- tokenize_characters(x, lowercase = lowercase,
+                               strip_non_alphanum = strip_non_alphanum)
+  out <- generate_ngrams_batch(chars, ngram_min = n_min, ngram_max = n,
+                               stopwords = "", ngram_delim = "")
+  if (!is.null(named)) names(out) <- named
+  simplify_list(out, simplify)
+}
---
+++ tests/testthat/test-shingles.R
@@ -0,0 +1,42 @@
+context("Shingle tokenizers")
+
+test_that("Character shingle tokenizer works as expected", {
+  out_l <- tokenize_character_shingles(docs_l, n = 3, n_min = 2)
+  out_c <- tokenize_character_shingles(docs_c, n = 3, n_min = 2)
+  out_1 <- tokenize_character_shingles(docs_c[1], n = 3, n_min = 2,
+                                       simplify = TRUE)
+
+  expect_is(out_l, "list")
+  expect_is(out_l[[1]], "character")
+  expect_is(out_c, "list")
+  expect_is(out_c[[1]], "character")
+  expect_is(out_1, "character")
+
+  expect_identical(out_l, out_c)
+  expect_identical(out_l[[1]], out_1)
+  expect_identical(out_c[[1]], out_1)
+
+  expect_named(out_l, names(docs_l))
+  expect_named(out_c, names(docs_c))
+
+  expect_error(tokenize_ngrams(bad_list))
+
+})
+
+test_that("Character shingle tokenizer produces correct output", {
+  phrase <- c("Remember who commended thy yellow stockings",
+              "And wished to see thee cross-gartered.")
+  names(phrase) <- c("Malvolio 1", "Malvolio 2")
+
+  out_d <- tokenize_character_shingles(phrase)
+  out_asis <- tokenize_character_shingles(phrase, lowercase = FALSE,
+                                          strip_non_alphanum = FALSE)
+
+  expect_identical(out_d[[1]][1:12], c("rem", "eme", "mem", "emb", "mbe", "ber",
+                                       "erw", "rwh", "who", "hoc", "oco", "com"))
+
+  expect_identical(out_asis[[2]][1:15], c("And", "nd ", "d w", " wi", "wis",
+                                          "ish", "she", "hed", "ed ", "d t",
+                                          " to", "to ", "o s", " se", "see"))
+
+})
