72d2cfac15893342a27a0fe9e4982de4e1ba63b2
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-04-01 11:18:14 -0400

    Fix imports in chunk text

--- R/chunk-text.R
+++ R/chunk-text.R
@@ -10,14 +10,14 @@
 #' @param doc_id The document IDs as a character vector. This will be taken from the names of the \code{x} vector if available. \code{NULL} is acceptable.
 #' @param ... Arguments passed on to \code{\link{tokenize_words}}.
 #' @examples
-#' chunked <- chunk_text(mobydick, chunk_size = 100)
+#' chunked <- chunk_text(mobydick, chunk_size = 1000)
 #' length(chunked)
 #' chunked[1:3]
 #' @export
 chunk_text <- function(x, chunk_size = 100, doc_id = names(x), ...) {
   check_input(x)
   stopifnot(chunk_size > 1)
-  if(is.character(x) && length(x) == 1) {
+  if (is.character(x) && length(x) == 1) {
     out <- chunk_individual_text(x = x, chunk_size = chunk_size,
                                  doc_id = doc_id, ...)
   } else {
@@ -37,12 +37,13 @@ chunk_individual_text <- function(x, chunk_size, doc_id, ...) {
   stopifnot(length(words) > chunk_size)
   chunks <- split(words, ceiling(seq_along(words)/chunk_size))
   if (!is.null(doc_id)) {
-    num_chars <- stringr::str_length(length(chunks))
-    chunk_ids <- stringr::str_pad(seq(length(chunks)), num_chars, pad = "0")
-    names(chunks) <- stringr::str_c(doc_id, chunk_ids, sep = "-")
+    num_chars <- stringi::stri_length(length(chunks))
+    chunk_ids <- stringi::stri_pad_left(seq(length(chunks)),
+                                       width = num_chars, pad = "0")
+    names(chunks) <- stringi::stri_c(doc_id, chunk_ids, sep = "-")
   } else {
     names(chunks) <- NULL
   }
-  out <- lapply(chunks, stringr::str_c, collapse = " ")
+  out <- lapply(chunks, stringi::stri_c, collapse = " ")
   out
 }
