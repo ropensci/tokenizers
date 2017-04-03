f7ebf96761d5c4a00c8a6544e15a44deb872ab46
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-04-03 13:07:53 -0400

    Tests for skip grams

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -7,3 +7,21 @@ names(docs_l) <- basename(paths)
 names(docs_c) <- basename(paths)
 
 bad_list <- list(a = paste(letters, collapse = " "), b = letters)
+
+# Using this sample sentence only because it comes from the paper where
+# skip n-grams are defined. Not my favorite sentence.
+input <- "Insurgents killed in ongoing fighting."
+
+bigrams <- c("insurgents killed", "killed in", "in ongoing", "ongoing fighting")
+
+skip2_bigrams <- c("insurgents killed", "insurgents in", "insurgents ongoing",
+                   "killed in", "killed ongoing", "killed fighting",
+                   "in ongoing", "in fighting", "ongoing fighting")
+
+trigrams <- c("insurgents killed in", "killed in ongoing", "in ongoing fighting")
+
+skip2_trigrams <- c("insurgents killed in", "insurgents killed ongoing",
+                    "insurgents killed fighting", "insurgents in ongoing",
+                    "insurgents in fighting", "insurgents ongoing fighting",
+                    "killed in ongoing", "killed in fighting",
+                    "killed ongoing fighting", "in ongoing fighting")
--- tests/testthat/test-ngrams.R
+++ tests/testthat/test-ngrams.R
@@ -68,14 +68,6 @@ test_that("Skip n-gram tokenizer works as expected", {
   expect_error(tokenize_skip_ngrams(bad_list))
 })
 
-test_that("Skip n-gram tokenizer produces correct output", {
-  # skip_on_os("windows")
-  out_1 <- tokenize_skip_ngrams(docs_c[1], n = 3, k = 2, simplify = TRUE)
-  expected <- c("chapter", "chapter 1", "chapter loomings",
-                "chapter call", "chapter 1 loomings", "chapter 1 call")
-  expect_identical(head(out_1, 6), expected)
-})
-
 test_that("Skip n-gram tokenizer consistently produces NAs where appropriate", {
   test <- c("This is a text", NA, "So is this")
   names(test) <- letters[1:3]
@@ -90,4 +82,69 @@ test_that("Skip n-gram tokenizer can use stopwords", {
   out <- tokenize_skip_ngrams(test, stopwords = "is", n = 2)
   expect_equal(length(out$a), 3)
   expect_identical(out$a[2], "this a")
+})
+
+test_that("Skips with values greater than k are refused", {
+  expect_false(check_width(c(0, 4, 5), k = 2))
+  expect_true(check_width(c(0, 3, 5), k = 2))
+  expect_false(check_width(c(0, 1, 3), k = 0))
+  expect_true(check_width(c(0, 1, 2), k = 0))
+  expect_false(check_width(c(0, 10, 11, 12), k = 5))
+  expect_true(check_width(c(0, 6, 11, 16, 18), k = 5))
+})
+
+test_that("Combinations for skip grams are correct", {
+  skip_pos <- get_valid_skips(2, 2)
+  expect_is(skip_pos, "list")
+  expect_length(skip_pos, 3)
+  expect_identical(skip_pos, list(c(0, 1), c(0, 2), c(0, 3)))
+
+  skip_pos2 <- get_valid_skips(3, 2)
+  expect_identical(skip_pos2, list(
+    c(0, 1, 2),
+    c(0, 1, 3),
+    c(0, 1, 4),
+    c(0, 2, 3),
+    c(0, 2, 4),
+    c(0, 2, 5),
+    c(0, 3, 4),
+    c(0, 3, 5),
+    c(0, 3, 6)))
+})
+
+test_that("Skip n-gram tokenizer works as expected", {
+  stopwords <- c("chapter", "me")
+  out_l <- tokenize_skip_ngrams(docs_l, n = 3, k = 2)
+  out_c <- tokenize_skip_ngrams(docs_c, n = 3, k = 2)
+  out_1 <- tokenize_skip_ngrams(docs_c[1], n = 3, k = 2, simplify = TRUE)
+
+  expect_is(out_l, "list")
+  expect_is(out_l[[1]], "character")
+  expect_is(out_c, "list")
+  expect_is(out_c[[1]], "character")
+  expect_is(out_1, "character")
+
+
+  expect_identical(out_l, out_c)
+  expect_identical(out_l[[1]], out_1)
+  expect_identical(out_c[[1]], out_1)
+
+  expect_named(out_l, names(docs_l))
+  expect_named(out_c, names(docs_c))
+
+  expect_error(tokenize_skip_ngrams(bad_list))
+})
+
+test_that("Skip n-gram tokenizer produces correct output", {
+  out_n2_k2 <- tokenize_skip_ngrams(input, n = 2, n_min = 2, k = 2, simplify = TRUE)
+  expect_equal(sort(skip2_bigrams), sort(out_n2_k2))
+  out_n3_k2 <- tokenize_skip_ngrams(input, n = 3, n_min = 3, k = 2, simplify = TRUE)
+  expect_equal(sort(skip2_trigrams), sort(out_n3_k2))
+})
+
+test_that("Skip n-gram tokenizers respects stopwords", {
+  out_1 <- tokenize_skip_ngrams("This is a sentence that is for the test.",
+                                n = 3, k = 2, stopwords = c("a", "the"),
+                                simplify = TRUE)
+  expect_equal(length(grep("the", out_1)), 0)
 })
